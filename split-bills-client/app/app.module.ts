646df67064217f4a7bee28df754add791c28db66
Natalia Govor <ngovor93@gmail.com>
2016-10-24 21:49:03 +0300

    setup new project

---
+++ split-bills-client/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }     from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HomeComponent } from './home.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    providers: [
+
+    ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
