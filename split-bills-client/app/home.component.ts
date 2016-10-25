ac08be4b1c3ba779bd242d6f5354f89426b14508
Natalia Govor <ngovor93@gmail.com>
2016-10-25 22:21:53 +0300

    added redirect for authorized users

--- split-bills-client/app/home.component.ts
+++ split-bills-client/app/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { Router }    from '@angular/router';
 
+import { AuthService } from './auth.service';
+
 @Component({
     template: `
         <h2>Home</h2>
         <p>Split Bills together with your friends!</p>
     `
 })
-export class HomeComponent { }
+export class HomeComponent { 
+    constructor(private authService: AuthService, private router: Router) {
+        if (this.authService.isLoggedIn) {
+            this.router.navigate(['/dashboard']);
+        }
+    }
+}
