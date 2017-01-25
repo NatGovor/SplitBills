4a695bdf7412576e70c75651cbb1f5bd98ab5f77
Natalia Govor <ngovor93@gmail.com>
2017-01-25 17:40:54 +0300

    fixed dashboard calculations, added styles for overall balances on dashboard

--- split-bills-client/app/secure-app/dashboard/dashboard.component.ts
+++ split-bills-client/app/secure-app/dashboard/dashboard.component.ts
@@ -16,16 +16,30 @@ import { MakePositivePipe } from '../../pipes/make-positive.pipe';
 @Component({
     template: `
         <h2>Dashboard</h2>
+        <div class="row total-header">
+            <div class="col-xs-4 total-balance">
+                <div>total balance</div>
+                <div  [ngClass]="getTotalClass()">{{ getTotalValue(finalResult) | currency:'USD':true:'1.2-2' }}</div>
+            </div>
+            <div class="col-xs-4 total-balance">
+                <div>you owe</div>
+                <div class="negative">{{ getTotalValue(finalDebts) | currency:'USD':true:'1.2-2' }}</div>
+            </div>
+            <div class="col-xs-4 total-balance">
+                <div>you are owed</div>
+                <div class="positive">{{ getTotalValue(finalCredits) | currency:'USD':true:'1.2-2' }}</div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-xs-6 negatives">
-                <div class="text-uppercase text-left"><b>You owe</b></div>
+                <div class="text-uppercase text-left balance-header"><b>You owe</b></div>
                 <div *ngFor="let res of finalDebts" class="balance">
                     <div>{{ res.friend.name }}</div>
                     <div class="negative">you owe {{ res.amount | makePositive | currency:'USD':true:'1.2-2' }}</div>
                 </div>
             </div>            
             <div class="col-xs-6 positives">
-                <div class="text-uppercase text-right"><b>You are owed</b></div>
+                <div class="text-uppercase text-right balance-header"><b>You are owed</b></div>
                 <div *ngFor="let res of finalCredits" class="balance">
                     <div>{{ res.friend.name }}</div>
                     <div class="positive">owes you {{ res.amount | makePositive | currency:'USD':true:'1.2-2' }}</div>
@@ -37,8 +51,23 @@ import { MakePositivePipe } from '../../pipes/make-positive.pipe';
         .negatives {
             border-right: 1px solid #eee;
         }
+        .balance-header {
+            border-bottom: 1px solid #eee;
+            padding: 5px 0;
+        }
         .balance {
-            margin-bottom: 10px;
+            margin: 5px 0 10px;
+        }
+        .total-header {
+            background-color: #eee;
+            text-align: center;
+            margin: 15px 0;
+        }
+        .total-header .total-balance {
+            border-left: 1px solid #888;
+        }
+        .total-header .total-balance:first-child {
+            border-left: none;
         }
     `]
 })
@@ -97,6 +126,8 @@ export class DashboardComponent implements OnInit {
                                     }
                                 });
 
+                                console.log(this.allBills);
+
                                 let myCredits = [];
                                 let myDebts = [];
                                 this.allBills.forEach(bill => {
@@ -106,6 +137,9 @@ export class DashboardComponent implements OnInit {
                                         myDebts.push(bill);
                                     }
                                 });
+
+                                console.log(myDebts);
+                                console.log(myCredits);
 
                                 var dashboardResult = {};
                                 myCredits.forEach(credit => {
@@ -117,21 +151,21 @@ export class DashboardComponent implements OnInit {
                                                 dashboardResult[debtor.userId] += debtor.amount;
                                             }
                                         }
-                                    })
-                                    
+                                    })                                    
                                 });
 
                                 myDebts.forEach(debt => {
                                     debt.debtors.forEach(debtor => {
-                                        if (debtor.userId !== this.currentUser.id) {
-                                            if (!dashboardResult[debtor.userId]) {
-                                                dashboardResult[debtor.userId] = -debtor.amount;
+                                        if (debtor.userId === this.currentUser.id) {
+                                            if (!dashboardResult[debt.paidBy]) {
+                                                dashboardResult[debt.paidBy] = -debtor.amount;
                                             } else {
-                                                dashboardResult[debtor.userId] -= debtor.amount;
+                                                dashboardResult[debt.paidBy] -= debtor.amount;
                                             }
                                         }
                                     });
                                 });
+                                console.log(dashboardResult);
 
                                 for (var key in dashboardResult) {
                                     this.finalResult.push(
@@ -146,5 +180,24 @@ export class DashboardComponent implements OnInit {
                             });
                     });
             });
+     }
+
+     getTotalValue(array) {
+         if (array) {
+             let total = 0;
+             array.forEach(el => {
+                 total += el.amount;
+             });
+
+             return total;
+         }
+     }
+
+     getTotalClass() {
+         if (this.getTotalValue(this.finalResult) >= 0) {
+             return 'positive';
+         } else {
+             return 'negative';
+         }
      }
 }
