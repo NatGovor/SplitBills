e9034fb1dfc4ee77625eff7eb8268d18e15acf73
Natalia Govor <ngovor93@gmail.com>
2017-03-09 15:28:42 +0300

    removed test projects for training

--- angular2-quickstart/app/admin/manage-heroes.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: `
-        <p>Manage yout heroes here</p>
-    `
-})
-export class ManageHeroesComponent {}
--- angular2-quickstart/app/crisis-center/crisis-center-home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: `
-        <p>Welcome to the Crisis Cener</p>
-    `
-})
-export class CrisisCenterHomeComponent { }
