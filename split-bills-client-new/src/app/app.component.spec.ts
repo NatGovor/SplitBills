13062401e001abc7d630263090fecc4eeb7656c9
Natalia Govor <ngovor93@gmail.com>
2017-03-30 17:08:14 +0300

    updated simple test

--- split-bills-client-new/src/app/app.component.spec.ts
+++ split-bills-client-new/src/app/app.component.spec.ts
@@ -1,18 +1,44 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 import { AppComponent } from './app.component';
 import { RouterLinkStubDirective } from '../testing/router-stubs';
 import { RouterOutletStubComponent } from '../testing/router-stubs';
-
-class RouterStub {
-  navigateByUrl(url: string) { return url; }
-}
+import { AuthService } from './unsecure-app/services/auth.service';
+import { HistoryService } from './common/services/history.service';
 
 let comp: AppComponent;
 let fixture: ComponentFixture<AppComponent>;
+
+let historyService: HistoryService;
+let historySpy: jasmine.Spy;
+
+class RouterStub {
+  public ne = new NavigationEnd(0, 'http://localhost:4200/login', 'http://localhost:4200/login');
+  public events = new Observable(observer => {
+    observer.next(this.ne);
+    observer.complete();
+  });
+
+  navigateByUrl(url: string) { return url; }
+}
+
+class AuthServiceSpy {
+  isLoggedIn = false;
+
+  login = jasmine.createSpy('login').and.callFake(
+    () => Promise
+      .resolve(true)
+      .then(() => Object.assign({}, true))
+  );
+
+  logout = jasmine.createSpy('logout').and.callFake(
+    () => true
+  );
+}
 
 describe('AppComponent & TestModule', () => {
   beforeEach(async(() => {
@@ -22,19 +48,35 @@ describe('AppComponent & TestModule', () => {
         RouterLinkStubDirective, RouterOutletStubComponent
       ],
       providers: [
-        { provide: Router, useClass: RouterStub }
+        { provide: Router, useClass: RouterStub },
+        HistoryService
       ]
+    })
+    .overrideComponent(AppComponent, {
+      set: {
+        providers: [
+          { provide: AuthService, useClass: AuthServiceSpy }
+        ]
+      }
     })
     .compileComponents()
     .then(() => {
       fixture = TestBed.createComponent(AppComponent);
       comp = fixture.componentInstance;
+
+      historyService = fixture.debugElement.injector.get(HistoryService);
+
+      historySpy = spyOn(historyService, 'addPage')
+            .and.returnValue(null);
+
     });
   }));
+
   tests();
 });
 
 function tests() {
+
   beforeEach(() => {
     fixture.detectChanges();
   });
